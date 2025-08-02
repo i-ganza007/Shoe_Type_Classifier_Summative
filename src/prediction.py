280dc766e80a3f5ef176ec79cf3647386637b9f8
i-ganza007 <i.ganza@alustudent.com>
2025-08-02 12:55:10 +0200

    Changed the threshold

--- src/prediction.py
+++ src/prediction.py
@@ -3,25 +3,22 @@ from tensorflow.keras.models import load_model
 from preprocessing import preprocess_image_for_prediction
 import os
 
+# Load the model
 model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "models", "second_80_percent_model_ian_g_cnn_model.h5")
 model = load_model(model_path)
 
+# Class labels
 class_labels = ['Boot', 'Sandal', 'Shoe']
 
-def predict_image(image_path, threshold=0.7):
+# Prediction function
+def predict_image(image_path):
     img_array = preprocess_image_for_prediction(image_path)
     predictions = model.predict(img_array)
-    
+
     predicted_index = np.argmax(predictions)
     confidence = float(np.max(predictions))
-
-    if confidence < threshold:
-        return {
-            "predicted_class": "Not a Shoe",
-            "confidence": confidence
-        }
-
     predicted_class = class_labels[predicted_index]
+
     return {
         "predicted_class": predicted_class,
         "confidence": confidence
